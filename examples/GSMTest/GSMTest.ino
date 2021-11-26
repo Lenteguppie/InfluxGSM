94e92d87b1ce1ca699a468f0b86b3569bf7a9b66
Sascha Vis <saschavis@gmail.com>
2021-11-26 09:13:53 +0100

    sync example

--- examples/GSMTest/GSMTest.ino
+++ examples/GSMTest/GSMTest.ino
@@ -1,3 +1,10 @@
+//#include <Arduino.h>
+/* #region Device configuration */
+#define VERSION_NUMBER "0.3"
+#define DEVICE_TOKEN "4ea2353a-fc4d-4463-b244-1279243b4396"
+/* #endregion */
+
+/* #region  GSM configuration */
 // set GSM PIN, if any
 #define GSM_PIN "0000"
 
@@ -6,14 +13,29 @@ const char apn[] = "live.vodafone.com"; //SET TO YOUR APN
 const char gprsUser[] = "vodafone";
 const char gprsPass[] = "vodafone";
 
-// Defined some constants for the tags
-#define VERSION_NUMBER "0.3"
-#define DEVICE_TOKEN "4ea2353a-fc4d-4463-b244-1279243b4396"
+// Range to attempt to autobaud
+#define GSM_AUTOBAUD_MIN 9600
+#define GSM_AUTOBAUD_MAX 115200
+#define TINY_GSM_USE_GPRS true
+#define TINY_GSM_USE_WIFI false
 
+#define TINY_GSM_TEST_GPRS true
+#define TINY_GSM_TEST_GPS true
+#define TINY_GSM_POWERDOWN true
+/* #endregion */
+
+/* #region  InfluxDB configuration */
+#if defined(ESP32)
+#include <WiFiMulti.h>
+WiFiMulti wifiMulti;
+#define DEVICE "ESP32"
+#elif defined(ESP8266)
+#include <ESP8266WiFiMulti.h>
+ESP8266WiFiMulti wifiMulti;
+#define DEVICE "ESP8266"
+#endif
 // InfluxDB v2 server url, e.g. https://eu-central-1-1.aws.cloud2.influxdata.com (Use: InfluxDB UI -> Load Data -> Client Libraries)
-#define INFLUXDB_URL "http://80.57.193.219"
-// InfluxDB server port, default 8086
-#define INFLUXDB_PORT 8086
+#define INFLUXDB_URL "http://80.57.193.219:8086"
 // InfluxDB v2 server or cloud API authentication token (Use: InfluxDB UI -> Data -> Tokens -> <select token>)
 #define INFLUXDB_TOKEN "bsDSnbA0CV5t8t39_0L4JWNC94GmUffo5iM2JzTsFLLwQjbSMGhUpN2NA8-Nv5pgSyjhjq6IcVT6CpC4BpJBxQ=="
 // InfluxDB v2 organization id (Use: InfluxDB UI -> User -> About -> Common Ids )
@@ -21,10 +43,15 @@ const char gprsPass[] = "vodafone";
 // InfluxDB v2 bucket name (Use: InfluxDB UI ->  Data -> Buckets)
 #define INFLUXDB_BUCKET "Smaddle device"
 
-String writePath, healthPath;
+// Set timezone string according to https://www.gnu.org/software/libc/manual/html_node/TZ-Variable.html
+// Examples:
+//  Pacific Time: "PST8PDT"
+//  Eastern: "EST5EDT"
+//  Japanesse: "JST-9"
+//  Central Europe: "CET-1CEST,M3.5.0,M10.5.0/3"
+#define TZ_INFO "CET-1CEST,M3.5.0,M10.5.0/3"
 
-#define uS_TO_S_FACTOR 1000000ULL // Conversion factor for micro seconds to seconds
-#define TIME_TO_SLEEP 60          // Time ESP32 will go to sleep (in seconds)
+/* #endregion */
 
 // Set serial for debug console (to the Serial Monitor, default speed 115200)
 #define SerialMon Serial
@@ -42,10 +69,13 @@ String writePath, healthPath;
 
 #include <TinyGsmClient.h>
 #include <ArduinoHttpClient.h>
-#include <DataPoint.h>
+#include "DataPoint.h"
 
 #include <SPI.h>
 #include <Ticker.h>
+
+TinyGsmClient client(modem);
+HttpClient influx(client, INFLUXDB_URL, INFLUXDB_PORT);
 
 #ifdef DUMP_AT_COMMANDS
 #include <StreamDebugger.h>
@@ -55,9 +85,13 @@ TinyGsm modem(debugger);
 TinyGsm modem(SerialAT);
 #endif
 
-TinyGsmClient client(modem);
-HttpClient influx(client, INFLUXDB_URL, INFLUXDB_PORT);
-// InfluxGSM influx(INFLUXDB_URL, INFLUXDB_PORT, client, INFLUXDB_BUCKET, INFLUXDB_ORG, INFLUXDB_TOKEN);
+
+// Data points
+DataPoint device("device");
+/* #endregion */
+
+#define uS_TO_S_FACTOR 1000000ULL // Conversion factor for micro seconds to seconds
+#define TIME_TO_SLEEP 60          // Time ESP32 will go to sleep (in seconds)
 
 #define UART_BAUD 9600
 #define PIN_DTR 25
@@ -69,7 +103,53 @@ HttpClient influx(client, INFLUXDB_URL, INFLUXDB_PORT);
 
 float lat, lon;
 
-DataPoint sensor("sensor");
+void constructEndpoints()
+{
+    //Construct writepath endpoint
+    writePath = "/api/v2/write";
+    writePath += "bucket=";
+    writePath += INFLUXDB_BUCKET;
+    writePath += "&org=";
+    writePath += INFLUXDB_ORG;
+    writePath += "&precision=ms";
+}
+
+bool sendPOST(String path, String data)
+{
+
+    influx.beginRequest();
+    influx.post(writePath);
+    influx.sendHeader("Content-Type", "application/json");
+    influx.sendHeader("Content-Length", data.length());
+    influx.sendHeader("Authorization", String("TOKEN " + String(INFLUXDB_TOKEN)));
+    influx.beginBody();
+    influx.print(data);
+    influx.endRequest();
+
+    // read the status code and body of the response
+    int statusCode = influx.responseStatusCode();
+    String response = influx.responseBody();
+
+    Serial.print("Status code: ");
+    Serial.println(statusCode);
+    Serial.print("Response: ");
+    Serial.println(response);
+
+    return statusCode == 204;
+}
+
+void sendSensorData()
+{
+    device.clearFields();
+
+    device.addField("lat", 1.3423);
+    device.addField("lon", 3.434);
+    device.addField("millis", millis());
+    if (!sendPOST(writePath, device.toLineProtocol()))
+    {
+        Serial.println("Something went wrong sending data to InfluxDB");
+    }
+}
 
 void enableGPS(void)
 {
@@ -229,53 +309,6 @@ void disconnectLTE()
 #endif
 }
 
-void constructEndpoints()
-{
-    //Construct writepath endpoint
-    writePath = "/write";
-    writePath += "bucket=";
-    writePath += INFLUXDB_BUCKET;
-    writePath += "&org=";
-    writePath += INFLUXDB_ORG;
-    writePath += "&precision=ms";
-}
-
-bool sendPOST(String path, String data)
-{
-
-    influx.beginRequest();
-    influx.post(writePath);
-    influx.sendHeader("Content-Type", "application/json");
-    influx.sendHeader("Content-Length", data.length());
-    influx.sendHeader("Authorization", String("TOKEN " + String(INFLUXDB_TOKEN)));
-    influx.beginBody();
-    influx.print(data);
-    influx.endRequest();
-
-    // read the status code and body of the response
-    int statusCode = influx.responseStatusCode();
-    String response = influx.responseBody();
-
-    Serial.print("Status code: ");
-    Serial.println(statusCode);
-    Serial.print("Response: ");
-    Serial.println(response);
-
-    return statusCode == 204;
-}
-
-void sendSensorData()
-{
-    sensor.clearFields();
-
-    sensor.addField("lat", 1.3423);
-    sensor.addField("lon", 3.434);
-    sensor.addField("millis", millis());
-    if(!sendPOST(writePath, sensor.toLineProtocol())){
-        Serial.println("Something went wrong sending data to InfluxDB");
-    }
-}
-
 void setup()
 {
     // Set console baud rate
@@ -287,24 +320,63 @@ void setup()
     pinMode(LED_PIN, OUTPUT);
     digitalWrite(LED_PIN, HIGH);
 
+    pinMode(PWR_PIN, OUTPUT);
+    digitalWrite(PWR_PIN, HIGH);
+    delay(300);
+    digitalWrite(PWR_PIN, LOW);
+
     modemPowerOn();
 
-    SerialAT.begin(UART_BAUD, SERIAL_8N1, PIN_RX, PIN_TX);
+    gsmInit();
+
+    client.setInsecure(true);
+
+    device.addTag("token", DEVICE_TOKEN);
+    device.addTag("version", VERSION_NUMBER);
 
     Serial.println("/**********************************************************/");
     Serial.println("To initialize the network test, please make sure your GPS");
     Serial.println("antenna has been connected to the GPS port on the board.");
     Serial.println("/**********************************************************/\n\n");
 
-    sensor.addTag("token", DEVICE_TOKEN);
-    sensor.addTag("version", VERSION_NUMBER);
-
     delay(10000);
 }
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
+    if (!modem.testAT())
+    {
+        Serial.println("Failed to restart modem, attempting to continue without restarting");
+        modemRestart();
+        return;
+    }
+
+    Serial.println("Start positioning . Make sure to locate outdoors.");
+    Serial.println("The blue indicator light flashes to indicate positioning.");
+
+    enableGPS();
+
+    while (1)
+    {
+        if (modem.getGPS(&lat, &lon))
+        {
+            Serial.println("The location has been locked, the latitude and longitude are:");
+            Serial.print("latitude:");
+            Serial.println(lat);
+            Serial.print("longitude:");
+            Serial.println(lon);
+            break;
+        }
+        digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+        delay(2000);
+    }
+
+    disableGPS();
+
+    Serial.println(F("/**********************************************************/"));
+    Serial.println(F("Sending the data to InfluxDB."));
+    Serial.println(F("/**********************************************************/\n\n"));
+
     connectLTE();
     sendSensorData();
     disconnectLTE();
